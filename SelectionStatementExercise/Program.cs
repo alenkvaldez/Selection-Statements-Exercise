190a4a709838950149736f862fe557bb3ff218c6
Alen Valdez <alenkvaldeztch@gmail.com>
2023-01-26 14:33:25 -0800

    initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Number guessing game
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine("try to guess my fave number");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is too low");
+            }
+            else if(userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is too high");
+            }
+            else(userInput > favNumber)
+            {
+                Console.WriteLine($"{userInput} you guessed it yayyyy");
+            }
         }
     }
 }
